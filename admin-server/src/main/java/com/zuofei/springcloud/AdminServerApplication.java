38279d1431d01c576d157c9a6b4f8f00e9cb34b4
飞 左 <348068347@qq.com>
2020-03-07 09:18:38 +0800

    spring-cloud-admin

--- admin-server/src/main/java/com/zuofei/springcloud/AdminServerApplication.java
+++ admin-server/src/main/java/com/zuofei/springcloud/AdminServerApplication.java
@@ -1,16 +1,16 @@
 package com.zuofei.springcloud;
 
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
-public class PaymentMain {
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain.class,args);
+        SpringApplication.run(AdminServerApplication.class,args);
     }
 }
