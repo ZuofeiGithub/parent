07f7f9006edbd5c47fb6f45bf8393cee287953a0
飞 左 <348068347@qq.com>
2020-03-08 17:41:56 +0800

    添加了登陆验证

---
+++ admin-service/src/main/java/com/zuofei/springcloud/config/SecuritySecureConfig.java
@@ -0,0 +1,34 @@
+package com.zuofei.springcloud.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+    private final String adminContextPath;
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter( "redirectTo" );
+        successHandler.setDefaultTargetUrl(adminContextPath + "/monitor");
+
+        http.authorizeRequests()
+                .antMatchers( adminContextPath + "/assets/**" ).permitAll()
+                .antMatchers( adminContextPath + "/login" ).permitAll()
+                //.antMatchers( adminContextPath + "/eureka" ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage( adminContextPath + "/login" ).successHandler( successHandler ).and()
+                .logout().logoutUrl( adminContextPath + "/logout" ).and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
+}
--- payment-service/src/main/java/com/zuofei/springcloud/PaymentMain.java
+++ payment-service/src/main/java/com/zuofei/springcloud/PaymentMain.java
@@ -3,10 +3,8 @@ package com.zuofei.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class PaymentMain {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class,args);
