d03c8c3a07db7db843879861499b839b16e3810e
飞 左 <348068347@qq.com>
2020-03-18 00:35:41 +0800

    Blog服务

---
+++ blog-service/src/main/java/com/zuofei/springcloud/aspect/LogAspect.java
@@ -0,0 +1,99 @@
+package com.zuofei.springcloud.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Pointcut("execution(* com.zuofei.springcloud.controller.*.*(..))")
+    public void log(){}
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint){
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog();
+        requestLog.setUrl(url);
+        requestLog.setIp(ip);
+        requestLog.setClassMethod(classMethod);
+        requestLog.setArgs(args);
+        logger.info("Request:{}", requestLog);
+    }
+
+    @After("log()")
+    public void doAfter(){
+        // logger.info("----doAfter----");
+    }
+
+
+    @AfterReturning(returning = "result",pointcut = "log()")
+    public void doAfterReturn(Object result){
+        logger.info("Result:{}",result);
+    }
+
+
+
+    private class RequestLog{
+        String url;
+        String ip;
+        String classMethod;
+        Object[] args;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getClassMethod() {
+            return classMethod;
+        }
+
+        public void setClassMethod(String classMethod) {
+            this.classMethod = classMethod;
+        }
+
+        public Object[] getArgs() {
+            return args;
+        }
+
+        public void setArgs(Object[] args) {
+            this.args = args;
+        }
+
+        @Override
+        public String toString() {
+            return "RequestLog{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    '}';
+        }
+    }
+}
---
+++ blog-service/src/main/java/com/zuofei/springcloud/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.zuofei.springcloud.controller;
+
+
+import com.zuofei.springcloud.exception.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+//        String blog = null;
+//        if(blog == null){
+//            throw  new NotFoundException("博客不存在");
+//        }
+        System.out.println("------index------");
+        return "index";
+    }
+}
